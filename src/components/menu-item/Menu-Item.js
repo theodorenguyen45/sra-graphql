cfc872ce86c15db6599586f5dcda3a4fe00a407d
Theodore Nguyen <theo@Theodores-MacBook-Pro.local>
2019-08-30 11:00:18 +1000

    Adding Router

--- src/components/menu-item/Menu-Item.js
+++ src/components/menu-item/Menu-Item.js
@@ -1,17 +1,22 @@
 import React from 'react';
+import { withRouter } from 'react-router-dom';
+
 import './Menu-Item.scss';
 
-export default ({ title, imageUrl, size }) => {
-  return (
-    <div className={`${size} menu-item`}>
-      <div
-        style={{ backgroundImage: `url(${imageUrl})` }}
-        className='background-image'
-      ></div>
-      <div className='content'>
-        <h1 className='title'>{title.toUpperCase()}</h1>
-        <span className='suptitle'>SHOP NOW</span>
-      </div>
+const MenuItems = ({ title, imageUrl, size, history, linkUrl, match }) => (
+  <div
+    className={`${size} menu-item`}
+    onClick={() => history.push(`${match.url}${linkUrl}`)}
+  >
+    <div
+      style={{ backgroundImage: `url(${imageUrl})` }}
+      className='background-image'
+    ></div>
+    <div className='content'>
+      <h1 className='title'>{title.toUpperCase()}</h1>
+      <span className='suptitle'>SHOP NOW</span>
     </div>
-  );
-};
+  </div>
+);
+
+export default withRouter(MenuItems);
