885d2a776e6841bb0abfde598879a812d2bb62a6
Theodore Nguyen <theo@Theodores-MacBook-Pro.local>
2019-09-05 10:19:36 +1000

    Adding Redirect after SignIn

--- src/commons/App.js
+++ src/commons/App.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Switch, Route } from 'react-router-dom';
+import { Switch, Route, Redirect } from 'react-router-dom';
 import { connect } from 'react-redux';
 import './App.css';
 import { auth, createUserProfileDocument } from 'firebase/firebase.utils';
@@ -7,7 +7,7 @@ import Homepage from 'pages/homepage/Home';
 import ShopPage from 'pages/shop/Shop';
 import SignInAndSignUpPage from 'pages/signin-and-signup/SignIn-SignUp';
 import Header from 'components/header/Header';
-import { setCurrentUser } from 'redux/user/user.action';
+import { setCurrentUser } from 'redux/user/user.actions';
 
 class App extends React.Component {
   unsubcribeFromAuth = null;
@@ -41,18 +41,32 @@ class App extends React.Component {
         <Switch>
           <Route exact path='/' component={Homepage} />
           <Route path='/shop' component={ShopPage} />
-          <Route path='/signin' component={SignInAndSignUpPage} />
+          <Route
+            exact
+            path='/signin'
+            render={() =>
+              this.props.currentUser ? (
+                <Redirect to='/' />
+              ) : (
+                <SignInAndSignUpPage />
+              )
+            }
+          />
         </Switch>
       </div>
     );
   }
 }
 
+const mapStateToProps = ({ user }) => ({
+  currentUser: user.currentUser
+});
+
 const mapDispatchToProps = dispatch => ({
   setCurrentUser: user => dispatch(setCurrentUser(user))
 });
 
 export default connect(
-  null,
+  mapStateToProps,
   mapDispatchToProps
 )(App);
